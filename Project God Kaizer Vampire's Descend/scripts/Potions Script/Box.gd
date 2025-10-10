65f025597ca525951d8c0efc425163d88810d8ff
Abdullah Khan <abdullahkhan200747@gmail.com>
2025-10-09 22:26:48 -0500

    added assets

---
+++ Project God Kaizer Vampire's Descend/scripts/Potions Script/Box.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+@onready var collision_shape = $CollisionShape2D
+@onready var sprite = $Sprite2D
+
+func _ready():
+	# Connect area entered for sword hits
+	var area = $Area2D  # Add a child Area2D for sword detection
+	area.area_entered.connect(_on_sword_hit)
+	
+	# Body entered will automatically work for character collision
+
+func _on_sword_hit(area: Area2D):
+	if area.get_collision_layer_value(2):
+		print("Hit by sword!")
+		queue_free()
